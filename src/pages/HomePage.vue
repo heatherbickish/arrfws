19a379470b26caacb5a88450b7d93d6539318494
heather bickish <hbickish@gmail.com>
2025-02-13 15:26:32 -0700

    kitten hero img and text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <!-- SECTION Hero img -->
+  <!-- SECTION Hero dog img -->
   <section class="container-fluid puppy-bg">
     <div class="row mt-4 justify-content-between">
       <div class="col-md-5">
@@ -15,7 +15,7 @@
       <div class="col-md-5 text-light">
         <div class="mt-5">
           <h5>OUR PURPOSE:</h5>
-          <h1 class="mt-5">
+          <h1 class="mt-5 dog-h1">
             The Animal Adoption <br> and Rescue Foundation <br> (AARF) exists to <br> improve the lives of <br>cats and
             dogs while <br> strengthening the <br> human-animal bond.
           </h1>
@@ -26,7 +26,7 @@
       </div>
     </div>
   </section>
-  e
+
   <!-- SECTION  Get Involved -->
   <section class="container-fluid">
     <div class="row mt-5 justify-content-center align-items-center">
@@ -47,7 +47,7 @@
     </div>
 
     <!-- SECTION Get Involved pics and text -->
-    <div class="row justify-content-center gap-2 mt-5">
+    <div class="row justify-content-center gap-2 mt-5 mb-5">
       <div class="col-md-3">
         <div class="mt-5">
           <img
@@ -67,7 +67,7 @@
             </p>
           </em>
         </div>
-        <div class="text-center mt-4">
+        <div class="text-center mt-5">
           <button class="btn rounded-pill bg-orange text-light py-2 px-3">LEARN MORE ABOUT ADOPTING</button>
         </div>
       </div>
@@ -88,7 +88,7 @@
             </p>
           </em>
         </div>
-        <div class="text-center mt-4">
+        <div class="text-center mt-5">
           <button class="btn rounded-pill bg-orange text-light py-2 px-3">LEARN MORE ABOUT FOSTERING</button>
         </div>
       </div>
@@ -109,19 +109,66 @@
             </p>
           </em>
         </div>
-        <div class="text-center mt-4">
+        <div class="text-center mt-5">
           <button class="btn rounded-pill bg-orange text-light py-2 px-3">LEARN MORE ABOUT VOLUNTEERING</button>
         </div>
       </div>
     </div>
   </section>
 
+  <!-- SECTION Hero cat img -->
+  <section class="container-fluid kitten-bg mt-5">
+    <div class="row justify-content-center align-items-center text-light">
+      <div class="col-md-4">
+        <div class="text-center">
+          <h1 class="kitten-h1">Party 4 Paws 2025 <br> To The Rescue</h1>
+        </div>
+        <div>
+          <b>
+            <p class="kitten-p p-2">This event helps AARF care for the many cats and dogs that journey through our
+              program
+              and, just as
+              importantly, raise awareness of AARF and its mission. Funds raised from the event will ensure that AARF
+              can continue to be a major rescue in Forsyth County that treats most health situations. This benefit is
+              every spring, but you can contribute year-round on our donate page. Our cats and dogs are always
+              appreciative! Thanks for helping in whatever way you can. <br><br> To donate, please go to the upper right
+              navigation and click DONATE. Thank you for helping.
+            </p>
+          </b>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div>
+          <img src="/public/img/kitten.png" alt="" class="kitten-img">
+        </div>
+      </div>
+    </div>
+  </section>
 
 </template>
 
 <style scoped lang="scss">
 .puppy-img {
   height: 40rem;
+}
+
+.kitten-img {
+  height: 55rem;
+}
+
+.involved-img {
+  height: 16rem;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+.dog-h1 {
+  line-height: 50px;
+}
+
+.kitten-h1 {
+  line-height: 70px;
 }
 
 .involved-p {
@@ -132,14 +179,7 @@
   line-height: 30px;
 }
 
-h1 {
-  line-height: 50px;
-}
-
-.involved-img {
-  height: 16rem;
-  width: 100%;
-  object-fit: cover;
-  object-position: center;
+.kitten-p {
+  line-height: 30px;
 }
 </style>
